654a77ff0b703edd893eb3621274a0ee9463568d
Horat1us <reclamme@gmail.com>
2023-11-29 17:17:30 +0100

    Add getLogin method to TokenEntity for backward compatibility

--- src/TwoFactor/TokenEntity.php
+++ src/TwoFactor/TokenEntity.php
@@ -8,18 +8,26 @@ use Wearesho\Yii2\Token;
 
 class TokenEntity extends Token\Entity
 {
+    private string $login;
+
     public function __construct(
         string $ip,
         string $login,
         string $value,
         \DateInterval $ttl
     ) {
+        $this->login = $login;
         parent::__construct(
             'login',
             static::generateOwner($ip, $login),
             $value,
             (new \DateTime())->add($ttl)
         );
+    }
+
+    public function getLogin(): string
+    {
+        return $this->login;
     }
 
     public static function generateOwner(string $ip, string $login): string
